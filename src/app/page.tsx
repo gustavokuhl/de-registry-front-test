bb5c3a795c86045fa0a355db219b3d7ee0feb449
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-03-23 00:50:28 -0300

    style: simple style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,34 @@ export default function Home() {
       nome: formData.get("nome"),
     }
 
+    await fetch("/", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(rawFormData),
+    })
+
     console.log(rawFormData)
   }
   return (
-    <main className="flex justify-center items-center">
-      <form action={onSubmit}>
-        <input type="text" name="nome" />
-        <input type="submit" />
-      </form>
+    <main className="flex flex-col gap-8 p-12">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-xl">Form</h1>
+        <form action={onSubmit}>
+          <div className="flex gap-2">
+            <input
+              type="text"
+              name="nome"
+              className="p-2 border-2 border-stone-300 rounded-md"
+            />
+            <input
+              type="submit"
+              className="p-2 border-2 border-stone-300 rounded-md cursor-pointer"
+            />
+          </div>
+        </form>
+      </div>
     </main>
   )
 }
