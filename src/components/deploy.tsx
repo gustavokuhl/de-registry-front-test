64775449767c9a3aee613eed9262890b7be9e99a
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-03-23 20:23:29 -0300

    feat: add new deploy contract button

--- src/components/deploy.tsx
+++ src/components/deploy.tsx
@@ -1,18 +1,39 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import Button from "./button"
 
 function Deploy() {
+  const [code, setCode] = useState("")
+
   async function clickButton() {
-    const rest = await fetch("/api", { method: "POST" })
-    console.log(await rest.json())
+    const resDeploy = await fetch("/api", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ solidityCode: code }),
+    })
+    console.log(resDeploy)
+    console.log(await resDeploy.json())
   }
 
+  useEffect(() => {
+    async function updateCode() {
+      const response = await fetch("/api/contracts", { method: "POST" })
+      const json = await response.json()
+      setCode(json.code)
+    }
+    updateCode()
+  }, [])
+
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       <h1 className="text-xl">Test API</h1>
+      <p>Verify your contract</p>
+      <textarea className="min-h-64" readOnly value={code} />
       <Button className="" onClick={clickButton}>
-        teste
+        Deploy on Otavio API
       </Button>
     </div>
   )
