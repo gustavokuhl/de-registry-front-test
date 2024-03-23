d84e0f0402aeb865c1902eedd7e9620a0d9ae3de
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-03-23 16:12:24 -0300

    feat: add contract call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,32 @@ export default function Home() {
   async function onSubmit(formData: FormData) {
     "use server"
 
-    const rawFormData = {
-      nome: formData.get("nome"),
-    }
+    const res = await fetch(
+      "https://deploycontractapi.fly.dev/compileAndDeploy",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          solidityCode:
+            "pragma solidity ^0.8.0; contract SimpleStorage { uint storedData; function set(uint x) public { storedData = x; } function get() public view returns (uint) { return storedData; }}",
+        }),
+      }
+    )
 
-    await fetch("/", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(rawFormData),
-    })
-
-    console.log(rawFormData)
+    console.log(await res.json())
   }
   return (
     <main className="flex flex-col gap-8 p-12">
       <div className="flex flex-col gap-2">
-        <h1 className="text-xl">Form</h1>
+        <h1 className="text-xl">Create contract</h1>
         <form action={onSubmit}>
-          <div className="flex gap-2">
-            <input
-              type="text"
-              name="nome"
-              className="p-2 border-2 border-stone-300 rounded-md"
-            />
-            <input
-              type="submit"
-              className="p-2 border-2 border-stone-300 rounded-md cursor-pointer"
-            />
-          </div>
+          <input
+            type="submit"
+            className="p-2 border-2 border-stone-300 rounded-md cursor-pointer"
+            value={"Create"}
+          />
         </form>
       </div>
     </main>
