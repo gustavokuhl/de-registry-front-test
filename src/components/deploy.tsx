2872260c3e3b554ac39943d07adb8d36e66d06e3
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-03-24 05:48:44 -0300

    feat: add wallet abstraction

--- src/components/deploy.tsx
+++ src/components/deploy.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { ConnectWallet, useWallet } from "@thirdweb-dev/react"
 import { useState } from "react"
 import Button from "./button"
 
@@ -12,6 +13,8 @@ function Deploy() {
   const [contractAddress, setContractAddress] = useState<string>("")
   const [pinataJsonAddress, setPinataJsonAddress] = useState<string>("")
   const [pinataPdfAddress, setPinataPdfAddress] = useState<string>("")
+
+  const userWallet = useWallet()
 
   async function fetchSampleContract() {
     const data = { pinataId: PINATA_CONTRACT_ID }
@@ -68,7 +71,7 @@ function Deploy() {
 
   async function saveOnScroll() {
     const relData = {
-      cidOwnerAddress: "af83f96944ceda6052AB0B9F78adFEb7661A6Ed7",
+      cidOwnerAddress: userWallet?.getAddress(),
       pdfCID: pinataPdfAddress,
       jsonCID: pinataJsonAddress,
     }
@@ -133,238 +136,247 @@ function Deploy() {
   return (
     <div className="flex flex-col gap-6">
       <h1 className="text-xl font-bold">Test API</h1>
-      <div className="flex flex-col gap-1">
-        <p>Verify your contract</p>
-        <Button onClick={fetchSampleContract}>
-          Fetch Example Contract from IPFS
-        </Button>
-        <textarea
-          className="min-h-64 bg-slate-200 rounded-lg p-4"
-          readOnly
-          value={code}
-        />
-        {code === "" ? null : (
-          <a
-            href={`https://gateway.ipfs.io/ipfs/${PINATA_CONTRACT_ID}`}
-            target="_blank"
-            className="text-xs"
-          >
-            See contract on IPFS
-          </a>
-        )}
-      </div>
-      <div className="flex flex-col gap-1">
-        <p>See the contract response</p>
-        <Button disabled={code === ""} className="" onClick={deployCode}>
-          {deploying ? "Deploying..." : "Deploy on Otavio API"}
-        </Button>
-        <textarea
-          readOnly
-          value={result}
-          className="min-h-64 bg-slate-200 rounded-lg p-4"
-        />
-        {contractAddress === "" ? null : (
-          <a
-            className="text-xs"
-            href={`https://sepolia.scrollscan.dev/address/${contractAddress}`}
-            target="_blank"
-          >
-            See contract on Scrollscan
-          </a>
-        )}
-      </div>
-      <div className="flex flex-col gap-1">
-        <p>Save the data on Pinata</p>
-        <form className="flex flex-col gap-2" action={saveOnPinata}>
-          <input
-            // required
-            className="bg-slate-200 rounded-lg p-2"
-            type="text"
-            value={contractAddress}
-            placeholder="contract address"
-            name="contractAddress"
-            readOnly
-          />
+      <ConnectWallet
+        switchToActiveChain={true}
+        modalSize={"compact"}
+        showThirdwebBranding={false}
+      />
+      {userWallet ? (
+        <>
           <div className="flex flex-col gap-1">
-            <p className="text-xs">Supply Agent</p>
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="text"
-              placeholder="name"
-              name="supplyAgent.name"
+            <p>Verify your contract</p>
+            <Button onClick={fetchSampleContract}>
+              Fetch Example Contract from IPFS
+            </Button>
+            <textarea
+              className="min-h-64 bg-slate-200 rounded-lg p-4"
+              readOnly
+              value={code}
             />
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="email"
-              placeholder="email"
-              name="supplyAgent.email"
-            />
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="text"
-              placeholder="cnpj"
-              name="supplyAgent.cnpj"
-            />
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="number"
-              placeholder="Inscrição Estadual"
-              name="supplyAgent.inscricaoEstadual"
-            />
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="number"
-              placeholder="Inscrição Municipal"
-              name="supplyAgent.inscricaoMunicipal"
-            />
+            {code === "" ? null : (
+              <a
+                href={`https://gateway.ipfs.io/ipfs/${PINATA_CONTRACT_ID}`}
+                target="_blank"
+                className="text-xs"
+              >
+                See contract on IPFS
+              </a>
+            )}
           </div>
           <div className="flex flex-col gap-1">
-            <p className="text-xs">Supply Area</p>
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="text"
-              placeholder="name"
-              name="supplyArea.name"
+            <p>See the contract response</p>
+            <Button disabled={code === ""} className="" onClick={deployCode}>
+              {deploying ? "Deploying..." : "Deploy on Otavio API"}
+            </Button>
+            <textarea
+              readOnly
+              value={result}
+              className="min-h-64 bg-slate-200 rounded-lg p-4"
             />
+            {contractAddress === "" ? null : (
+              <a
+                className="text-xs"
+                href={`https://sepolia.scrollscan.dev/address/${contractAddress}`}
+                target="_blank"
+              >
+                See contract on Scrollscan
+              </a>
+            )}
           </div>
           <div className="flex flex-col gap-1">
-            <p className="text-xs">Physical Address</p>
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="text"
-              placeholder="cep"
-              name="supplyAgent.address.cep"
-            />
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="text"
-              placeholder="street"
-              name="supplyAgent.address.street"
-            />
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="text"
-              placeholder="city"
-              name="supplyAgent.address.city"
-            />
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="text"
-              placeholder="state"
-              name="supplyAgent.address.state"
-            />
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="text"
-              placeholder="complement"
-              name="supplyAgent.address.complement"
-            />
-          </div>
+            <p>Save the data on Pinata</p>
+            <form className="flex flex-col gap-2" action={saveOnPinata}>
+              <input
+                // required
+                className="bg-slate-200 rounded-lg p-2"
+                type="text"
+                value={contractAddress}
+                placeholder="contract address"
+                name="contractAddress"
+                readOnly
+              />
+              <div className="flex flex-col gap-1">
+                <p className="text-xs">Supply Agent</p>
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="text"
+                  placeholder="name"
+                  name="supplyAgent.name"
+                />
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="email"
+                  placeholder="email"
+                  name="supplyAgent.email"
+                />
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="text"
+                  placeholder="cnpj"
+                  name="supplyAgent.cnpj"
+                />
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="number"
+                  placeholder="Inscrição Estadual"
+                  name="supplyAgent.inscricaoEstadual"
+                />
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="number"
+                  placeholder="Inscrição Municipal"
+                  name="supplyAgent.inscricaoMunicipal"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <p className="text-xs">Supply Area</p>
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="text"
+                  placeholder="name"
+                  name="supplyArea.name"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <p className="text-xs">Physical Address</p>
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="text"
+                  placeholder="cep"
+                  name="supplyAgent.address.cep"
+                />
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="text"
+                  placeholder="street"
+                  name="supplyAgent.address.street"
+                />
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="text"
+                  placeholder="city"
+                  name="supplyAgent.address.city"
+                />
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="text"
+                  placeholder="state"
+                  name="supplyAgent.address.state"
+                />
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="text"
+                  placeholder="complement"
+                  name="supplyAgent.address.complement"
+                />
+              </div>
 
-          <div className="flex flex-col gap-1">
-            <p className="text-xs">Contract Data</p>
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="number"
-              placeholder="duration"
-              name="duration"
-            />
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="number"
-              placeholder="since"
-              name="since"
-            />
-            <input
-              // required
-              disabled={contractAddress === ""}
-              className="bg-slate-200 rounded-lg p-2"
-              type="text"
-              placeholder="unitsMeasure"
-              name="unitsMeasure"
-            />
-          </div>
+              <div className="flex flex-col gap-1">
+                <p className="text-xs">Contract Data</p>
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="number"
+                  placeholder="duration"
+                  name="duration"
+                />
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="number"
+                  placeholder="since"
+                  name="since"
+                />
+                <input
+                  // required
+                  disabled={contractAddress === ""}
+                  className="bg-slate-200 rounded-lg p-2"
+                  type="text"
+                  placeholder="unitsMeasure"
+                  name="unitsMeasure"
+                />
+              </div>
 
-          <Button
-            // disabled={contractAddress === "" || pinataJsonAddress !== ""}
-            type="submit"
-          >
-            Save on Pinata
-          </Button>
-        </form>
-        <Button
-          disabled={pinataJsonAddress === ""}
-          onClick={() =>
-            window.open(
-              "https://gateway.ipfs.io/ipfs/" + pinataJsonAddress,
-              "_blank",
-              "noopener"
-            )
-          }
-        >
-          Show on Pinata
-        </Button>
-      </div>
-      <div className="flex flex-col gap-1">
-        <p>Sign your document with the off-chain version</p>
-        <form action={handleSignFile} className="flex flex-col gap-1">
-          <input
-            // required
-            type="file"
-            name="signFile"
-            className="bg-slate-200 rounded-lg p-2"
-            accept="application/pdf"
-          />
-          <Button type="submit">Save on Pinata</Button>
-        </form>
-        <Button
-          disabled={pinataPdfAddress === ""}
-          onClick={() => {
-            window.open(
-              "https://gateway.ipfs.io/ipfs/" + pinataPdfAddress,
-              "_blank",
-              "noopener"
-            )
-          }}
-        >
-          View on Pinata
-        </Button>
-      </div>
-      <div className="flex flex-col gap-1">
-        <p>Save the IPFS data on Scroll</p>
-        <Button
-          onClick={saveOnScroll}
-          disabled={pinataPdfAddress === "" || pinataJsonAddress === ""}
-        >
-          Save data on Scroll
-        </Button>
-      </div>
+              <Button
+                // disabled={contractAddress === "" || pinataJsonAddress !== ""}
+                type="submit"
+              >
+                Save on Pinata
+              </Button>
+            </form>
+            <Button
+              disabled={pinataJsonAddress === ""}
+              onClick={() =>
+                window.open(
+                  "https://gateway.ipfs.io/ipfs/" + pinataJsonAddress,
+                  "_blank",
+                  "noopener"
+                )
+              }
+            >
+              Show on Pinata
+            </Button>
+          </div>
+          <div className="flex flex-col gap-1">
+            <p>Sign your document with the off-chain version</p>
+            <form action={handleSignFile} className="flex flex-col gap-1">
+              <input
+                // required
+                type="file"
+                name="signFile"
+                className="bg-slate-200 rounded-lg p-2"
+                accept="application/pdf"
+              />
+              <Button type="submit">Save on Pinata</Button>
+            </form>
+            <Button
+              disabled={pinataPdfAddress === ""}
+              onClick={() => {
+                window.open(
+                  "https://gateway.ipfs.io/ipfs/" + pinataPdfAddress,
+                  "_blank",
+                  "noopener"
+                )
+              }}
+            >
+              View on Pinata
+            </Button>
+          </div>
+          <div className="flex flex-col gap-1">
+            <p>Save the IPFS data on Scroll</p>
+            <Button
+              onClick={saveOnScroll}
+              disabled={pinataPdfAddress === "" || pinataJsonAddress === ""}
+            >
+              Save data on Scroll
+            </Button>
+          </div>
+        </>
+      ) : null}
     </div>
   )
 }
